8677b7ad9642b1009acc95a4fd0e0cc55fcac25a
Rakib Hassan <mailrakibhassan@gmail.com>
2025-06-29 15:39:13 +0600

    (feat) added pre middleware, static methods, instance methods to the userSchema

--- src/app/modules/user/user.schema.ts
+++ src/app/modules/user/user.schema.ts
@@ -1,5 +1,6 @@
-import { model, Schema } from "mongoose";
+import { CallbackError, model, Schema } from "mongoose";
 import { User as IUser } from "./user.interface";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema<IUser>(
   {
@@ -18,5 +19,38 @@ const userSchema = new Schema<IUser>(
     timestamps: true, // automatically adds createdAt and updatedAt
   }
 );
+
+// hash the password before storing into db
+userSchema.pre("save", async function (next) {
+  try {
+    this.password = await bcrypt.hash(
+      this.password,
+      Number(process.env.BCRYPT_SALT_ROUNDS)
+    );
+    // after successful hasing proceed saving data to db
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  }
+});
+
+// make the password field empty for after save response
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
+});
+
+// user instance methods
+userSchema.methods.checkPassword = async function (plainTextPassword: string) {
+  // 'this' referes to the user doc
+  // this.password is a hashed password
+  return bcrypt.compare(plainTextPassword, this.password);
+};
+
+// static methods
+userSchema.statics.getUserByUsername = async function (username: string) {
+  // this refers to the User model
+  return this.findOne({ username });
+};
 
 export const User = model<IUser>("User", userSchema);
